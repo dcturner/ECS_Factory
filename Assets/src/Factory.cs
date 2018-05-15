450ee87732be3960b0086992b3ad817ccfc04f35
DcTurner <mrdcturner@gmail.com>
2018-05-15 03:12:20 +0100

    Scheduler started - workshop tasks incoming

--- Assets/src/Factory.cs
+++ Assets/src/Factory.cs
@@ -1,118 +1,147 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor.Drawers;
 using UnityEngine;
 
+public enum FactoryMode
+{
+    OOP,
+    DOD
+}
+
 public class Factory : SerializedMonoBehaviour
 {
-	public static Factory INSTANCE;
-	private int tick = 0;
-	[PropertyRange(0.0000001f, 1f)]
-	public float tickRate = 0.1f;
-	private float t;
-	
-	public Storage HD, RAM, L3;
-	private Storage[] storage;
-	private int storageCount;
-	
-	// ORDER
-	// Quantity of each design to be built
-	public Dictionary<VehicleDesign, int> vehicleOrder;
-	public Dictionary<VehicleDesign, int> CompletedVehicles;
-	private Dictionary<VehiclePart_Config, int> requiredParts;
-	private Dictionary<VehiclePart_Config, List<VehiclePart>> parts;
+    public static Factory INSTANCE;
+    public FactoryMode factoryMode;
+    private int tick = 0;
+    [PropertyRange(0.0000001f, 1f)] public float tickRate = 0.1f;
+    private float t;
 
-	private void Awake()
-	{
-		INSTANCE = this;
-		t = tickRate;
-		storage = FindObjectsOfType<Storage>();
-		storageCount = storage.Length;
-		requiredParts = new Dictionary<VehiclePart_Config, int>();
-		parts = new Dictionary<VehiclePart_Config, List<VehiclePart>>();
-		CompletedVehicles = new Dictionary<VehicleDesign, int>();
-	}
+    public Storage HD, RAM, L3;
+    private Storage[] storage;
+    private List<Workshop> workshops;
+  
+    private int storageCount;
 
-	private void Start()
-	{
-		// get total number of parts required
-		foreach (KeyValuePair<VehicleDesign,int> _DESIGN_PAIR in vehicleOrder)
-		{
-			VehicleDesign _DESIGN = _DESIGN_PAIR.Key;
-			int _TOTAL = _DESIGN_PAIR.Value;
-			
-			// Set number of completed vehicles to zero
-			CompletedVehicles[_DESIGN] = 0;
-			
-			Debug.Log(_DESIGN.designName +" -- " + _TOTAL);
-			foreach (KeyValuePair<VehiclePart_Config,int> _PartCount in _DESIGN.quantities)
-			{
-				VehiclePart_Config _partType = _PartCount.Key;
-				if (requiredParts.ContainsKey(_partType))
-				{
-					requiredParts[_partType]+= _DESIGN.quantities[_partType] * _TOTAL;
-				}
-				else
-				{
-					requiredParts.Add(_partType, _DESIGN.quantities[_partType] * _TOTAL);
-				}
-			}
-		}
-		
-		// summarise order and sling it into RAM
-		Debug.Log("Part totals...");
-		int index = 0;
-		foreach (KeyValuePair<VehiclePart_Config,int> _PAIR in requiredParts)
-		{
-			VehiclePart_Config _PART = _PAIR.Key;
-			int _TOTAL = _PAIR.Value;
-			GameObject _PART_PREFAB = _PART.prefab_part;
-			parts.Add(_PART, new List<VehiclePart>());
-			List<VehiclePart> _LIST = parts[_PART];
-			Debug.Log(_PART.name + " x " + _TOTAL);
+    // ORDER
+    // Quantity of each design to be built
+    public Dictionary<VehicleDesign, int> vehicleOrder;
+    public Dictionary<VehicleDesign, int> CompletedVehicles;
+    private Dictionary<VehiclePart_Config, int> requiredParts;
+    private Dictionary<VehiclePart_Config, List<VehiclePart>> parts;
 
-			
-			for (int i = 0; i < _TOTAL; i++)
-			{
-				GameObject _part_OBJ = (GameObject) Instantiate(_PART_PREFAB, new Vector3(i*0.05f, 0f, 0f), Quaternion.identity);
-				_LIST.Add( _part_OBJ.GetComponent<VehiclePart>());
-				if (i < RAM.capacity)
-				{
-					_part_OBJ.transform.position = RAM.storageLocations[index];
-					index++;
-				}
-			}
-		}
-	}
+    private void Awake()
+    {
+        INSTANCE = this;
+        t = tickRate;
+        storage = FindObjectsOfType<Storage>();
+        workshops = FindObjectsOfType<Workshop>().OrderBy(m => m.workshopIndex).ToList();
+        storageCount = storage.Length;
+        requiredParts = new Dictionary<VehiclePart_Config, int>();
+        parts = new Dictionary<VehiclePart_Config, List<VehiclePart>>();
+        CompletedVehicles = new Dictionary<VehicleDesign, int>();
+    }
 
-	private void Update()
-	{
-		if(Timer.TimerReachedZero(ref t))
-		{
-			Tick();
-		}
-	}
+    private void Start()
+    {
+        Get_RequiredParts();
+        ScheduleTasks();
+    }
 
-	private void Tick()
-	{
-		t = tickRate;
-		tick++;
+    private void Get_RequiredParts()
+    {
+        // get total number of parts required
+        foreach (KeyValuePair<VehicleDesign, int> _DESIGN_PAIR in vehicleOrder)
+        {
+            VehicleDesign _DESIGN = _DESIGN_PAIR.Key;
+            int _TOTAL = _DESIGN_PAIR.Value;
 
-		for (int i = 0; i < storageCount; i++)
-		{
-			storage[i].Tick();
-		}
-	}
-	
-	#region ------------------------- < GIZMOS METHODS
+            // Set number of completed vehicles to zero
+            CompletedVehicles[_DESIGN] = 0;
 
-	[Button("Toggle Storage Cell Gizmos")]
-	public void GIZMOS_storage_showCells()
-	{
-		Storage.GIZMOS_DRAW_CELLS = !Storage.GIZMOS_DRAW_CELLS;
-	}
+            Debug.Log(_DESIGN.designName + " -- " + _TOTAL);
+            foreach (KeyValuePair<VehiclePart_Config, int> _PartCount in _DESIGN.quantities)
+            {
+                VehiclePart_Config _partType = _PartCount.Key;
+                if (requiredParts.ContainsKey(_partType))
+                {
+                    requiredParts[_partType] += _DESIGN.quantities[_partType] * _TOTAL;
+                }
+                else
+                {
+                    requiredParts.Add(_partType, _DESIGN.quantities[_partType] * _TOTAL);
+                }
+            }
+        }
 
-	#endregion ------------------------ GIZMOS METHODS >
+        // summarise order and sling it into RAM
+        Debug.Log("Part totals...");
+        int index = 0;
+        foreach (KeyValuePair<VehiclePart_Config, int> _PAIR in requiredParts)
+        {
+            VehiclePart_Config _PART = _PAIR.Key;
+            int _TOTAL = _PAIR.Value;
+            GameObject _PART_PREFAB = _PART.prefab_part;
+            parts.Add(_PART, new List<VehiclePart>());
+            List<VehiclePart> _LIST = parts[_PART];
+            Debug.Log(_PART.name + " x " + _TOTAL);
 
+
+            for (int i = 0; i < _TOTAL; i++)
+            {
+                GameObject _part_OBJ =
+                    (GameObject) Instantiate(_PART_PREFAB, new Vector3(i * 0.05f, 0f, 0f), Quaternion.identity);
+                _LIST.Add(_part_OBJ.GetComponent<VehiclePart>());
+                if (i < RAM.capacity)
+                {
+                    _part_OBJ.transform.position = RAM.storageLocations[index];
+                    index++;
+                }
+            }
+        }
+    }
+
+    private void ScheduleTasks()
+    {
+        switch (factoryMode)
+        {
+            case FactoryMode.OOP:
+                
+                break;
+            case FactoryMode.DOD:
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        if (Timer.TimerReachedZero(ref t))
+        {
+            Tick();
+        }
+    }
+
+    private void Tick()
+    {
+        t = tickRate;
+        tick++;
+
+        for (int i = 0; i < storageCount; i++)
+        {
+            storage[i].Tick();
+        }
+    }
+
+    #region ------------------------- < GIZMOS METHODS
+
+    [Button("Toggle Storage Cell Gizmos")]
+    public void GIZMOS_storage_showCells()
+    {
+        Storage.GIZMOS_DRAW_CELLS = !Storage.GIZMOS_DRAW_CELLS;
+    }
+
+    #endregion ------------------------ GIZMOS METHODS >
 }
--- Assets/src/Workshop.cs
+++ Assets/src/Workshop.cs
@@ -9,11 +9,60 @@ public class Workshop : MonoBehaviour
 	public float width, height;
 	public Storage L2, L1, REG;
 
+	private List<WorkshopTask> tasklist;
+
+	private void Awake()
+	{
+		tasklist = new List<WorkshopTask>();
+	}
+
+	public void AddTask(VehicleDesign _design, VehiclePart_Config _vehiclePart, int _iterations, bool _appendOnCompletion)
+	{
+		List<VehicleDesign_RequiredPart> _partList = new List<VehicleDesign_RequiredPart>();
+		foreach (VehicleDesign_RequiredPart _PART in _design.requiredParts)
+		{
+			if (_PART.partConfig == _vehiclePart)
+			{
+				_partList.Add(_PART);
+			}
+		}
+		tasklist.Add(new WorkshopTask(_design, _partList.ToArray(),_iterations, _appendOnCompletion));
+	}
+
 	private void OnDrawGizmos()
 	{
 		if (L2 && L1 && REG)
 		{
 			GizmoHelpers.DrawRect(Color.cyan, transform.position, width, height, "CORE [" + workshopIndex + "]");
 		}
+	}
+
+	private void NextTask()
+	{
+		
+	}
+
+	private void Tick()
+	{
+	}
+
+	private void RequestPart()
+	{
+	}
+}
+
+public struct WorkshopTask
+{
+	public VehicleDesign design;
+	public VehicleDesign_RequiredPart[] requiredParts;
+	public bool appendOnCompletion;
+	public int iterations;
+
+	public WorkshopTask(VehicleDesign _design, VehicleDesign_RequiredPart[] _requiredParts, int _iterations, bool _appendOnCompletion)
+	{
+		design = _design;
+		requiredParts = _requiredParts;
+		iterations = _iterations;
+		appendOnCompletion = _appendOnCompletion;
 	}
 }
